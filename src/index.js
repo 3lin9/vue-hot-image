c26c2e61027cce8ef86b49f1fb39cc6aa9d85f06
3lin9 <ep_ling@163.com>
2024-10-28 16:41:34 +0800

    mbug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import HotZone from './components/HotZone.vue';
 import './assets/styles/main.scss';
+import add from './directives/add';
+import move from './directives/move';
+import resize from './directives/resize';
 
 const app = createApp(HotZone)
 app.directive('addArea', add)
